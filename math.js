90df34805997adaffd4508ba8f1b951b790c506f
Hannah Walsh <walsh3hm@dukes.jmu.edu>
2016-04-27 19:23:33 -0400

    Added files via upload

---
+++ math.js
@@ -0,0 +1,19 @@
+// export a function for doing addition
+exports.add = function(a, b) {
+	return a + b;
+};
+
+// export a function for doing subtraction
+exports.subtract = function(a, b) {
+	return a - b;
+};
+
+// export a function for doing multiplication
+exports.multiply = function(a, b) {
+	return a * b;
+};
+
+// export a function for doing division
+exports.divide = function(a, b) {
+	return a / b;
+};
